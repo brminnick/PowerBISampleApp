8b5eec453c4e4550efe2c0ecdd499950eee9c83c
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-01-26 16:52:10 +0000

    Added links to readme

--- PowerBISampleApp/Constants/AzureConstants.cs
+++ PowerBISampleApp/Constants/AzureConstants.cs
@@ -2,9 +2,13 @@
 {
 	public static class AzureConstants
 	{
-		public const string ApplicationId = "f797e4b7-5a4f-4684-bb4b-520d005511d2";
-		public const string RedirectURL = "https://PowerBISampleApp.azurewebsites.net/.auth/login/done";
-		public const string Resource = "https://analysis.windows.net/powerbi/api";
+#warning Update this constant with the Azure AD ApplicationId for your Native App Registration: https://github.com/brminnick/PowerBISampleApp#applicationid
+        public const string ApplicationId = "f797e4b7-5a4f-4684-bb4b-520d005511d2";
+
+#warning Update this constant with the Azure AD Redirect Url for your Native App Registration: https://github.com/brminnick/PowerBISampleApp#redirect-url
+        public const string RedirectURL = "https://PowerBISampleApp.azurewebsites.net/.auth/login/done";
+		
+        public const string Resource = "https://analysis.windows.net/powerbi/api";
 
 		public const string OAuth2Authority = "https://login.microsoftonline.com/common";
 	}
